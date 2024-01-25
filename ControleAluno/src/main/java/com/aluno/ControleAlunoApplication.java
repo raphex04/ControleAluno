71539be9694b6884a384e00904d37425e9563e49
raphex04 <132359802+raphex04@users.noreply.github.com>
2024-01-25 16:41:07 -0300

    Add files via upload

---
+++ ControleAluno/src/main/java/com/aluno/ControleAlunoApplication.java
@@ -0,0 +1,13 @@
+package com.aluno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleAlunoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleAlunoApplication.class, args);
+	}
+
+}
